d700cc7101e93d54ed094c2007cef1d0d327ed6a
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-10-30 12:01:32 +0500

    Basic express app set up + Create userRoutes and userControllers file

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(port, () => {
+    console.log("API Running....");
+});
